93b46cc81196b5f9b1c92ded63f2ac358071bedf
Telinc1 <telinc13@gmail.com>
2025-11-09 12:28:21 +0200

    Create tests for bps.ts

---
+++ tests/bps.test.js
@@ -0,0 +1,139 @@
+import * as assert from "node:assert/strict";
+import {describe, test} from "node:test";
+
+import {
+    crc32,
+    applyBPS,
+    MalformedPatchError,
+    WrongInputChecksumError,
+    WrongInputSizeError,
+    getExpectedChecksum,
+} from "../dist/index.js";
+
+import {readFile} from "./utils/file.js";
+
+function assertBytesEqual(actual, expected) {
+    assert.strictEqual(actual.length, expected.length);
+
+    for (let i = 0; i < actual.length; i++) {
+        assert.strictEqual(actual[i], expected[i]);
+    }
+}
+
+describe("bps", () => {
+    describe("getExpectedChecksum", () => {
+        test("a_to_a.bps", () => {
+            const a = readFile("./data/a.txt");
+            const patch = readFile("./data/a_to_a.bps");
+
+            assert.strictEqual(getExpectedChecksum(patch), crc32(a));
+        });
+
+        test("b_to_a.bps", () => {
+            const b = readFile("./data/b.txt");
+            const patch = readFile("./data/b_to_a.bps");
+
+            assert.strictEqual(getExpectedChecksum(patch), crc32(b));
+        });
+
+        test("ff4lunar_to_ff4chocobo.bps", () => {
+            const ff4lunar = readFile("./data/ff4lunar.spc");
+            const patch = readFile("./data/ff4lunar_to_ff4chocobo.bps");
+
+            assert.strictEqual(getExpectedChecksum(patch), crc32(ff4lunar));
+        });
+    });
+
+    test("a_to_a.bps", () => {
+        const patch = readFile("./data/a_to_a.bps");
+
+        const base = readFile("./data/a.txt");
+        const expected = base;
+
+        assertBytesEqual(applyBPS(base, patch), expected);
+    });
+
+    test("source_read.bps", () => {
+        const patch = readFile("./data/source_read.bps");
+
+        const base = readFile("./data/a.txt");
+        const expected = base;
+
+        assertBytesEqual(applyBPS(base, patch), expected);
+    });
+
+    test("target_read.bps", () => {
+        const patch = readFile("./data/target_read.bps");
+
+        const base = readFile("./data/b.txt");
+        const expected = readFile("./data/a_small.txt");
+
+        assertBytesEqual(applyBPS(base, patch), expected);
+    });
+
+    test("source_copy.bps", () => {
+        const patch = readFile("./data/source_copy.bps");
+
+        const base = readFile("./data/a.txt");
+        const expected = readFile("./data/ba.txt");
+
+        assertBytesEqual(applyBPS(base, patch), expected);
+    });
+
+    test("target_copy.bps", () => {
+        const patch = readFile("./data/target_copy.bps");
+
+        const base = readFile("./data/b.txt");
+        const expected = readFile("./data/a.txt");
+
+        assertBytesEqual(applyBPS(base, patch), expected);
+    });
+
+    test("a_to_a_malformed.bps", () => {
+        const patch = readFile("./data/a_to_a_malformed.bps");
+
+        const base = readFile("./data/a.txt");
+
+        assert.throws(() => applyBPS(base, patch), MalformedPatchError);
+    });
+
+    test("a_to_a.bps (wrong input size)", () => {
+        const patch = readFile("./data/a_to_a.bps");
+
+        const actualBase = readFile("./data/a.txt");
+        const wrongBase = new Uint8Array([1, 2, 3]);
+
+        const expected = new WrongInputSizeError(actualBase.length, wrongBase.length);
+
+        assert.throws(() => applyBPS(wrongBase, patch), expected);
+    });
+
+    test("a_to_a.bps (wrong input file)", () => {
+        const patch = readFile("./data/a_to_a.bps");
+
+        const actualBase = readFile("./data/a.txt");
+        const wrongBase = readFile("./data/b.txt");
+
+        const expected = new WrongInputChecksumError(crc32(actualBase), crc32(wrongBase));
+
+        assert.throws(() => applyBPS(wrongBase, patch), expected);
+    });
+
+    test("b_to_a.bps", () => {
+        const patch = readFile("./data/b_to_a.bps");
+
+        const base = readFile("./data/b.txt");
+        const expected = readFile("./data/a.txt");
+
+        assertBytesEqual(applyBPS(base, patch), expected);
+    });
+
+    test("ff4lunar_to_ff4chocobo.bps", () => {
+        const patch = readFile("./data/ff4lunar_to_ff4chocobo.bps");
+
+        const base = readFile("./data/ff4lunar.spc");
+        const expected = readFile("./data/ff4chocobo.spc");
+
+        assertBytesEqual(applyBPS(base, patch), expected);
+    });
+});
